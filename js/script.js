c224ece324052aa36efafcd43ca763ad72ab12bd
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-14 15:54:16 +0000

    Volume of a Sphere

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kukwac
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates volume of a sphere.
+ */
+function calculate() {
+  // input
+    let radius = parseFloat(document.getElementById('radius').value);
+    const PI = Math.PI
+  // process
+    let volume = (4/3) * PI * radius**3;
+
+  // output
+  document.getElementById('volume').innerHTML = 'The volume is: ' + volume.toFixed(2) + 'cm3' + '.'
+}
